eeaf9b746050ab298ee45a80b54c0cb47312fa5a
nostromo76 <ivans1976@gmail.com>
2023-07-21 11:24:41 +0200

    2nd commit testing home page

--- cypress/e2e/contrast.cy.js
+++ cypress/e2e/contrast.cy.js
@@ -1,5 +1,6 @@
 const { beforeEach } = require("mocha")
 
+
 describe('Test of Web page Contrast as company portfolio and work', () => {
   beforeEach('Open page', () => {
     cy.visit('../../index.html')
@@ -97,13 +98,13 @@ describe('Test of Web page Contrast as company portfolio and work', () => {
 
   })
   it('should display the rotate animation on the secondary layer', function () {
-   
+
     cy.clock(); // start a fake timer
-    cy.get('#rotate').should('have.text', 'simple'); 
-    cy.tick(1000); 
-    cy.get('#rotate').should('have.text', 'simple'); 
+    cy.get('#rotate').should('have.text', 'simple');
     cy.tick(1000);
-    cy.get('#rotate').should('have.text', 'simple'); 
+    cy.get('#rotate').should('have.text', 'simple');
+    cy.tick(1000);
+    cy.get('#rotate').should('have.text', 'simple');
   });
   /*it('should display the rotate animation on the secondary layer', function () {
 cy.visit('index.html');
@@ -115,44 +116,78 @@ cy.get('.secondary_layer').toMatchImageSnapshot({ name: 'creative' }); // take a
 });*/
 
 
-it('Test h3 middle headlines',function(){
-  cy.get('.services > .container > h3').should('be.visible') 
-})
-
-it('Test pictures in feature box and its texts',function(){
-  
-  cy.get('.feature-box > .img-responsive')
-  .should('be.visible')
-  .and(($img) => {
-  
-  $img.each((index, image) => {
-
-  expect(image.naturalHeight).to.be.within(400, 600)
+  it('Test h3 middle headlines', function () {
+    cy.get('.services > .container > h3').should('be.visible')
   })
+
+  it('Test pictures in feature box and its texts', function () {
+
+    cy.get('.feature-box > .img-responsive')
+      .should('be.visible')
+      .and(($img) => {
+
+        $img.each((index, image) => {
+
+          expect(image.naturalHeight).to.be.within(400, 600)
+        })
+      })
   })
-})
-it('Test text below pictures',function(){
-  cy.get(':nth-child(1) > .feature-body > h4').should('have.text','Business Consultancy')
-  cy.get(':nth-child(2) > .feature-body > h4').should('have.text','Chartered Accountancy')
-  cy.get(':nth-child(3) > .feature-body > h4').should('have.text','Financial Analysis')
+  it('Test text below pictures', function () {
+    cy.get(':nth-child(1) > .feature-body > h4').should('have.text', 'Business Consultancy')
+    cy.get(':nth-child(2) > .feature-body > h4').should('have.text', 'Chartered Accountancy')
+    cy.get(':nth-child(3) > .feature-body > h4').should('have.text', 'Financial Analysis')
 
 
-})
-it('Test of counter effect',function(){
-  cy.reload()
-  cy.wait(4000)
-  cy.get(':nth-child(1) > .counter-number').should ('exist', '10000')
-  cy.get(':nth-child(1) > .counter-text').should('have.text','CLIENTS')
-  cy.get(':nth-child(1) > .counter-text').should ('exist', '500')
-  cy.get(':nth-child(2) > .counter-text').should('have.text','ACCOUNTANTS')
-  cy.get(':nth-child(3) > .counter-number').should ('exist', '7500')
-  cy.get(':nth-child(3) > .counter-text').should('have.text','PROJECTS')
-  cy.get(':nth-child(4) > .counter-number').should ('exist', '2500')
-  cy.get(':nth-child(4) > .counter-text').should('have.text','ANALYSTS')
+  })
+  it('Test of counter effect', function () {
+    cy.reload()
+    cy.wait(4000)
+    cy.get(':nth-child(1) > .counter-number').should('exist', '10000')
+    cy.get(':nth-child(1) > .counter-text').should('have.text', 'CLIENTS')
+    cy.get(':nth-child(1) > .counter-text').should('exist', '500')
+    cy.get(':nth-child(2) > .counter-text').should('have.text', 'ACCOUNTANTS')
+    cy.get(':nth-child(3) > .counter-number').should('exist', '7500')
+    cy.get(':nth-child(3) > .counter-text').should('have.text', 'PROJECTS')
+    cy.get(':nth-child(4) > .counter-number').should('exist', '2500')
+    cy.get(':nth-child(4) > .counter-text').should('have.text', 'ANALYSTS')
 
 
-})
-})
+  })
+
+  it('Test Container scheme', function () {
+    cy.get('.projects > .container > h3').should('be.visible').and('have.text', 'Projects')
+    cy.get('.section-left, .section-right').should('be.visible');
+    cy.get('.section-left .heading h5').should('be.visible').and('have.text', 'Designed a new websiteCreated our own app');
+    cy.get('.section-left .heading .fa-desktop').should('be.visible');
+    cy.get('.section-left .heading p').should('be.visible').and('have.text', 'Lorem ipsum dolor sit amet, quas augue inciderint ius ad, mea sumo decore conceptam ei.Lorem ipsum dolor sit amet, quas augue inciderint ius ad, mea sumo decore conceptam ei.');
+    //2 section
+    cy.get('.section-right .heading h5').should('be.visible').and('have.text', 'Invested in a new start up');
+    cy.get('.section-right .heading .fa-credit-card').should('be.visible');
+    cy.get('.section-right .heading p').should('be.visible').and('have.text', 'Lorem ipsum dolor sit amet, quas augue inciderint ius ad, mea sumo decore conceptam ei.');
+    //3 section
+    cy.get(':nth-child(4) > .col-lg-5 > .article > .wrap > .heading > h5').should('be.visible').and('have.text', 'Created our own app');
+    cy.get(':nth-child(4) > .col-lg-5 > .article > .wrap > .heading > h5 > .fa').should('be.visible');
+    cy.get(':nth-child(4) > .col-lg-5 > .article > .wrap > .heading > p').should('be.visible').and('have.text', 'Lorem ipsum dolor sit amet, quas augue inciderint ius ad, mea sumo decore conceptam ei.');
+    cy.get('.button .btn').should('be.visible');
+
+  })
+  it.only('Test client slider', function () {
+    cy.get('.clients-carousel').scrollIntoView()
+    cy.get('.active > .row > :nth-child(1) > .thumbnail > img').should('have.attr', 'src', 'images/clients/client1.png')
+    cy.get('.active > .row > :nth-child(2) > .thumbnail > img').should('have.attr', 'src', 'images/clients/client2.png')
+    cy.get('.active > .row > :nth-child(3) > .thumbnail > img').should('have.attr', 'src', 'images/clients/client3.png')
+    cy.get('.active > .row > :nth-child(4) > .thumbnail > img').should('have.attr', 'src', 'images/clients/client4.png')
+    cy.wait(6000)
+    cy.get('.active > .row > :nth-child(1) > .thumbnail > img').should('have.attr', 'src', 'images/clients/client5.png')
+    cy.get('.active > .row > :nth-child(2) > .thumbnail > img').should('have.attr', 'src', 'images/clients/client6.png')
+    cy.get('.active > .row > :nth-child(3) > .thumbnail > img').should('have.attr', 'src', 'images/clients/client7.png')
+    cy.get('.active > .row > :nth-child(4) > .thumbnail > img').should('have.attr', 'src', 'images/clients/client8.png')
+
+
+  })
+});
+
+
 
 
 
